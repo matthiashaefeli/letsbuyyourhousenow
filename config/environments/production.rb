eb9b4ca831ab76aec36e6de8c4c408969adedc1e
matthias haefeli <haefeli.matthias@gmail.com>
2020-04-30 18:34:19 -0600

    change text, add gitignore gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'example.com' #replace with your own url
+  host = 'www.letsbuyyourhousenow.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "lets-buy-your-house-now@gmail.com",
-    :password             => "ngtzmeqjjwvfbmjq",
+    :user_name            => ENV["USERNAME"],
+    :password             => ENV["PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
