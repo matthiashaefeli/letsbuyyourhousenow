de55b833b940f69da21e8574303aa02b418e11a2
matthias haefeli <haefeli.matthias@gmail.com>
2020-06-11 15:43:12 -0600

    cant login test

--- spec/system/login_spec.rb
+++ spec/system/login_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe 'Login', type: :system do
       expect(page).to have_text('SignOut')
     end
   end
+
+  describe 'user login' do
+    it 'cant login if user does not exists' do
+      visit new_user_session_path
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: '123456'
+      click_on 'Log in'
+      expect(current_path).to eq('/users/sign_in')
+    end
+  end
 end
